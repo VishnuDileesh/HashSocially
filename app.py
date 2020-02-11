5c938e4a6aa75eb918f4baf6080dab4ed94945d2
Pheyonix <dev@vishnudileesh.com>
2020-02-11 17:16:50 +0530

    Add templates and static folder

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/sign-up')
+def sign_up():
+
+    return render_template('signup.html')
+
+@app.route('/sign-in')
+def sign_in():
+
+    return render_template('signin.html')
 
 
