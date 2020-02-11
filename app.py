a4db0d616423c966a52ea4ee2e617a6c0a59dae9
Pheyonix <dev@vishnudileesh.com>
2020-02-11 17:30:10 +0530

    Get inputs from the forms via post method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,13 +7,32 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
+
+    if request.method == 'POST':
+
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print(username)
+        print(email)
+        print(password)
+
 
     return render_template('signup.html')
 
-@app.route('/sign-in')
+@app.route('/sign-in', methods=["GET", "POST"])
 def sign_in():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print(email)
+        print(password)
 
     return render_template('signin.html')
 
