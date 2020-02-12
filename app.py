fecd2509b1ee2aa9532dba01d283947cbde8b56d
Pheyonix <dev@vishnudileesh.com>
2020-02-12 17:53:59 +0530

    Add database configuration and User model

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "hashsocially.db"))
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'myveryverysecretlysecretsecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(1000))
+
 
 @app.route('/')
 def index():
@@ -37,3 +58,5 @@ def sign_in():
     return render_template('signin.html')
 
 
+if __name__ == '__main__':
+    app.run()
